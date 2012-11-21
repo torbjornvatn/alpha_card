2d67da25502d51a173fab306e601b3b9b18f0005
Torbjørn Vatn <torbjorn@vatn.org>
2012-11-21 15:39:39 +0100

    Exclude jayq from noir-cljs dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject alpha-card "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://exampl.com/FIXME"
-  :dependencies [[noir-cljs "0.3.6" :exclusions [org.clojure/clojure]]
-                 [jayq "0.2.3-SNAPSHOT"]
-                 ;[jayq "0.2.2"]
+  :dependencies [[noir-cljs "0.3.6" :exclusions [org.clojure/clojure jayq]]
+                 [jayq "0.2.2"]
                  [crate "0.2.1"]
                  [noir "1.3.0-beta10"]
                  [org.clojure/clojure "1.4.0"]]
